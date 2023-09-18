ffd4d1b8901b1ac81cdad124a4a7306e26647b35
adandemarco <adanxd97@gmail.com>
2023-09-18 20:19:10 -0300

    agrego métodos atacar en las clases Estado, Pokemon y Habilidad y también recibirDaño en Pokemon con su lógica

--- PokemonAlgo3/src/main/java/org/example/habilidad/Habilidad.java
+++ PokemonAlgo3/src/main/java/org/example/habilidad/Habilidad.java
@@ -1,5 +1,25 @@
 package org.example.habilidad;
 
-public class Habilidad{
+import org.example.Elemento.Element;
+import org.example.pokemon.Pokemon;
 
+public class Habilidad{
+    String nombre;
+    Double multiplicador_mismo_tipo;
+    Element elemento;
+    Integer poder_de_ataque;
+    Integer cantidad_de_usos;
+    Double mismoTipo;
+    public Double getMultiplicadorMismotipo(Element elemento){
+        if (this.elemento.get_tipo() == elemento.get_tipo()){
+            return 1.5; // mismoTipo
+        }
+        else{
+            return 1.0;
+        }
+    }
+    public void atacar(Pokemon pokemon /*pokemon a atacar*/, Element element /*elemento del pokemon que está atacando*/){
+        mismoTipo = this.getMultiplicadorMismotipo(element);
+        pokemon.recibirDanio(poder_de_ataque, mismoTipo, elemento, pokemon.getAtaque());
+    }
 }
