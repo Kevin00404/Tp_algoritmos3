11e980d8b1d598a32dc75d97f6105d80b873c446
KevinGitp <vallejokev10@gmail.com>
2023-09-23 19:37:10 -0300

    agrego clase pokebola, no terminada. Y agrego implementacion a Entrenador

--- PokemonAlgo3/src/main/java/org/example/Entrenador.java
+++ PokemonAlgo3/src/main/java/org/example/Entrenador.java
@@ -1,4 +1,35 @@
 package org.example;
 
 public class Entrenador {
+    List<Pokemon> pokebola;
+    Pokemon pokemonActual;
+
+
+    public Entrenador(List<Pokemon> pokebola, Pokemon pokemonActual) {
+        this.pokebola = pokebola;
+        this.pokemonActual = pokemonActual;
+    }
+
+    public Entrenador(List<Pokemon> pokebola) {
+        this.pokebola = pokebola;
+    }
+
+    //esto se cambia. Se hace una clase de pokebola.
+    public Entrenador(Pokemon pokemon1,Pokemon pokemon2,Pokemon pokemon3, Pokemon pokemon4, Pokemon pokemon5, Pokemon pokemon6) {
+        for
+        this.pokebola = pokebola;
+    }
+
+
+    public Pokemon getPokemonActual() {
+        return pokemonActual;
+    }
+
+    public void setPokemonActual(Pokemon pokemonActual) {
+        this.pokemonActual = pokemonActual;
+    }
+
+
+
 }
+
---
+++ PokemonAlgo3/src/main/java/org/example/Pokebola.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Pokebola {
+    List<Pokemon> pokebola;
+
+    public Pokebola(List<Pokemon> pokebola) {
+        this.pokebola = pokebola;
+    }
+
+    final int POKEMONES_POR_POKEBOLA=6;
+
+    public void agregarPokemon(Pokemon pokemon){
+        if pokebola.lenght<POKEMONES_POR_POKEBOLA{
+            this.pokebola.add(pokemon)
+        }
+        System.out.println("Error:Pokebola ya contiene "+POKEMONES_POR_POKEBOLA + " pokemones.");
+    }
+    public void quitarPokemon(Pokemon pokemon){
+        if pokebola.//contiene a pokemon//
+
+    }
+
+    public void cantidad(){
+        System.out.println(POKEMONES_POR_POKEBOLA);
+    }
+
+
+}
+
