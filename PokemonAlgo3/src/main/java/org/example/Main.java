e6bc1b86579ff0b7753bbb843d2e4d087c868b2a
William Ramirez <86168463+WARFiuba@users.noreply.github.com>
2023-11-14 15:14:51 -0300

    principio de json

--- PokemonAlgo3/src/main/java/org/example/Main.java
+++ PokemonAlgo3/src/main/java/org/example/Main.java
@@ -1,13 +1,16 @@
 package org.example;
 
 import org.example.Elemento.*;
+import org.example.JSON.LeerJSON;
 import org.example.habilidad.HabilidadAtaque;
 import org.example.pokemon.Pokemon;
 
 public class Main {
     public static void main(String[] args) {
 
-        Juego juego = new Juego();
-        juego.iniciar_Juego();
+        //Juego juego = new Juego();
+        //juego.iniciar_Juego();
+        LeerJSON leer = new LeerJSON();
+        leer.leerJson();
     }
 }
