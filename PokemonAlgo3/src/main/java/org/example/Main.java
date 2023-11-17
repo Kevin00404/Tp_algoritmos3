93348b1bfc4796b06fce42ef2ac06323731ae9c9
William Ramirez <86168463+WARFiuba@users.noreply.github.com>
2023-11-17 15:23:12 -0300

    JSON arreglado en parte
    funcionamiento parcial de JSON

--- PokemonAlgo3/src/main/java/org/example/Main.java
+++ PokemonAlgo3/src/main/java/org/example/Main.java
@@ -1,16 +1,18 @@
 package org.example;
 
-import org.example.Elemento.*;
-import org.example.JSON.LeerJSON;
-import org.example.habilidad.HabilidadAtaque;
-import org.example.pokemon.Pokemon;
+import org.example.JSON.LeerArchivoJson;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //Juego juego = new Juego();
         //juego.iniciar_Juego();
-        LeerJSON leer = new LeerJSON();
-        leer.leerJson();
+        LeerArchivoJson leer = new LeerArchivoJson();
+        leer.LeerJson();
     }
 }
